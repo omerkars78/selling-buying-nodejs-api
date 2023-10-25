a4c8763c605534570bf0660e110e91cfe58f7f54
Ömer Kars <81978907+omerkars78@users.noreply.github.com>
2023-10-25 11:06:43 +0300

    Some New Methods in ProductService

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,43 +1,236 @@
 import Product from '../models/Product';
+import { Op } from 'sequelize';
 
 class ProductService {
-    // Kullanıcı oluşturma
-    static async createProduct(userData: any): Promise<Product> {
-        return await Product.create(userData);
-    }
 
-// Kullanıcı bilgilerini güncelleme
-static async updateProduct(productId: number, updateData: any): Promise<number> {
-    const affectedCount = await Product.update(updateData, {
-        where: {
-            id: productId
-        }
-    });
-    return affectedCount[0];
-}
-
-
-
-    // Kullanıcıyı ID ile al
-    static async getProductById(productId: number): Promise<Product | null> {
-        return await Product.findByPk(productId);
-    }
-
-    // Kullanıcıyı e-posta ile al
-    static async geProductByPrice(price: string): Promise<Product | null> {
-        return await Product.findOne({
-            where: {
-                price:price
-            }
-        });
-    }
-
-    // Tüm kullanıcıları listele
+    // get all product
     static async getAllProducts(): Promise<Product[]> {
         return await Product.findAll();
     }
 
-    // Kullanıcıyı sil
+    // get product by id ASC
+    static async getProductByIdAsc(productId: number): Promise<Product | null> {
+        return await Product.findOne({
+            where: {
+                productId: productId
+            },
+            order: [
+                ['productId', 'ASC']
+            ]
+        });
+    }
+
+    // get product by id DESC
+    static async getProductByIdDesc(productId: number): Promise<Product | null> {
+        return await Product.findOne({
+            where: {
+                productId: productId
+            },
+            order: [
+                ['productId', 'DESC']
+            ]
+        });
+    }
+
+    // get product by price 
+    static async geProductByPrice(price: string): Promise<Product | null> {
+        return await Product.findOne({
+            where: {
+                price: price
+            },
+        });
+    }
+
+    // get product by price ASC 
+    static async geProductByPriceAsc(price: string): Promise<Product | null> {
+        return await Product.findOne({
+            where: {
+                price: price
+            },
+            order: [
+                ['price', 'ASC']
+            ]
+        });
+    }
+    // get product by price DESC 
+    static async geProductByPriceDesc(price: string): Promise<Product | null> {
+        return await Product.findOne({
+            where: {
+                price: price
+            },
+            order: [
+                ['price', 'ASC']
+            ]
+        });
+    }
+
+    // get product between price ASC
+    static async getProductBetweenPriceAsc(minPrice: number, maxPrice: number): Promise<Product[]> {
+        const products = await Product.findAll({
+            where: {
+                price: {
+                    [Op.gte]: minPrice,  // Greater than or equal to start date
+                    [Op.lte]: maxPrice     // Less than or equal to end date
+                }
+            },
+            order: [
+                ['price', 'ASC']
+            ]
+        });
+
+        return products;
+    }
+    // get product between price DESC
+    static async getProductBetweenPriceDesc(minPrice: number, maxPrice: number): Promise<Product[]> {
+        const products = await Product.findAll({
+            where: {
+                price: {
+                    [Op.gte]: minPrice,  // Greater than or equal to start date
+                    [Op.lte]: maxPrice     // Less than or equal to end date
+                }
+            },
+            order: [
+                ['price', 'DESC']
+            ]
+        });
+
+        return products;
+    }
+    // get product by createdAt ASC
+    static async getProductByCreatedAtAsc(createdAt: Date): Promise<Product[]> {
+        const products = await Product.findAll({
+            where: {
+                createdAt: createdAt
+            },
+            order: [
+                ['createdAt', 'ASC']
+            ]
+        });
+
+        return products;
+    }
+    // get product by createdAt DESC
+    static async getProductByCreatedAtDesc(createdAt: Date): Promise<Product[]> {
+        const products = await Product.findAll({
+            where: {
+                createdAt: createdAt
+            },
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        });
+
+        return products;
+    }
+    // get product by createdAt DESC since a given date
+    static async getProductSinceCreatedAtAsc(createdAt: Date): Promise<Product[]> {
+        const products = await Product.findAll({
+            where: {
+                createdAt: {
+                    [Op.gte]: createdAt  // Greater than or equal to given date
+                }
+            },
+            order: [
+                ['createdAt', 'ASC']
+            ]
+        });
+
+        return products;
+    }
+    // get product by createdAt DESC since a given date
+    static async getProductSinceCreatedAtDesc(createdAt: Date): Promise<Product[]> {
+        const products = await Product.findAll({
+            where: {
+                createdAt: {
+                    [Op.gte]: createdAt  // Greater than or equal to given date
+                }
+            },
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        });
+
+        return products;
+    }
+
+    // get product by createdAt ASC between two dates
+    static async getProductBetweenCreatedAtAsc(startDate: Date, endDate: Date): Promise<Product[]> {
+        const products = await Product.findAll({
+            where: {
+                createdAt: {
+                    [Op.gte]: startDate,  // Greater than or equal to start date
+                    [Op.lte]: endDate     // Less than or equal to end date
+                }
+            },
+            order: [
+                ['createdAt', 'ASC']
+            ]
+        });
+
+        return products;
+    }
+
+    // get product by createdAt DESC between two dates
+    static async getProductBetweenCreatedAtDesc(startDate: Date, endDate: Date): Promise<Product[]> {
+        const products = await Product.findAll({
+            where: {
+                createdAt: {
+                    [Op.gte]: startDate,  // Greater than or equal to start date
+                    [Op.lte]: endDate     // Less than or equal to end date
+                }
+            },
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        });
+
+        return products;
+    }
+
+    // get product by updatedAt ASC
+    static async getProductByUpdatedAtAsc(updatedAt: Date): Promise<Product[]> {
+        const products = await Product.findAll({
+            where: {
+                updatedAt: updatedAt
+            },
+            order: [
+                ['updatedAt', 'ASC']
+            ]
+        });
+
+        return products;
+    }
+
+    // get product by updatedAt ASC
+    static async getProductByUpdatedAtDesc(updatedAt: Date): Promise<Product[]> {
+        const products = await Product.findAll({
+            where: {
+                updatedAt: updatedAt
+            },
+            order: [
+                ['updatedAt', 'DESC']
+            ]
+        });
+
+        return products;
+    }
+
+    // create product
+    static async createProduct(userData: any): Promise<Product> {
+        return await Product.create(userData);
+    }
+
+    // update product
+    static async updateProduct(productId: number, updateData: any): Promise<number> {
+        const affectedCount = await Product.update(updateData, {
+            where: {
+                id: productId
+            }
+        });
+        return affectedCount[0];
+    }
+
+    // delete product
     static async deleteProduct(productId: number): Promise<void> {
         await Product.destroy({
             where: {
@@ -46,7 +239,6 @@ static async updateProduct(productId: number, updateData: any): Promise<number> 
         });
     }
 
-    // Diğer fonksiyonları da buraya ekleyebilirsiniz.
 }
 
 export default ProductService;
